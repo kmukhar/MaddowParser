8d3ee3c3eff4c09225ee1e6806c02b9a4d8555c4
kmukhar <kevin.mukhar@gmail.com>
2014-01-02 10:29:36 -0800

    copied from vogella

---
+++ MaddowParser/src/de/vogella/rss/tests/ReadTest.java
@@ -0,0 +1,17 @@
+package de.vogella.rss.tests;
+
+import de.vogella.rss.model.Feed;
+import de.vogella.rss.model.FeedMessage;
+import de.vogella.rss.read.RSSFeedParser;
+
+public class ReadTest {
+	public static void main(String[] args) {
+		RSSFeedParser parser = new RSSFeedParser(
+				"http://www.msnbc.com/feeds/latest");
+		Feed feed = parser.readFeed();
+		System.out.println(feed);
+		for (FeedMessage message : feed.getMessages()) {
+			System.out.println(message);
+		}
+	}
+}
